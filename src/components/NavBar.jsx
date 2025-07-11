5f66e8c149f32af9366763f321131ec15b5ce6d8
ahmed alaa <ahmedalaa@ahmeds-MacBook-Pro.local>
2025-07-11 03:05:24 +0300

    animate the hero section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,7 +19,7 @@ const NavBar = () => {
         backgroundColor: "#00000050",
         backdgroundFilter: "blur(10px)",
         duration: 1,
-        ease: "power1.inOut",
+        ease: "back.inOut",
       }
     );
   });
